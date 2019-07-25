395e0f75a5ac7914aca3eb28c237f41851e8d087
rockluoye <1661829205@qq.com>
2019-07-25 21:37:35 +0800

    1.创建了userapp，

---
+++ star/config.py
@@ -0,0 +1,12 @@
+"""一些额外的配置信息"""
+
+YZX_URL = 'https://open.ucpaas.com/ol/sms/sendsms'
+
+YZX_PARAMAS = {
+    "sid": "5a1308393e422fcd047938c88d8a425e",
+    "token": "6ec4cb99a9143ab94d4054783048a633",
+    "appid": "706be1f5d2794d20be092e9f6ec554d6",
+    "templateid": "442650",
+    "param": None,
+    "mobile": None,
+}
